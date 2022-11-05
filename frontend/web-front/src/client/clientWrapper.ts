c47923679f025ca899b95d08d91aee274420c486
chou-dai <c.daiki0916@gmail.com>
2022-11-05 10:22:51 +0900

    prod setting

--- frontend/web-front/src/client/clientWrapper.ts
+++ frontend/web-front/src/client/clientWrapper.ts
@@ -3,9 +3,7 @@ import {
 } from "./api";
 import { Configuration } from "./configuration";
 
-// dev環境
-// const basePath = "http://133.18.238.196:8080";
-const basePath = "http://localhost:8080";
+const basePath = "http://133.18.238.196:8080";
 
 const option = {};
 
