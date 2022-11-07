f8f51cae286100af1891e94f11f656a7a5bca9d9
chou-dai <c.daiki0916@gmail.com>
2022-11-07 20:20:56 +0900

    modified nginx

--- frontend/web-front/src/client/clientWrapper.ts
+++ frontend/web-front/src/client/clientWrapper.ts
@@ -3,8 +3,6 @@ import {
 } from "./api";
 import { Configuration } from "./configuration";
 
-// const basePath = "http://133.18.238.196:8080";
-
 const protocol = location.protocol;
 const hostname = location.hostname;
 const basePath = protocol + "//" + hostname + ":8080";
